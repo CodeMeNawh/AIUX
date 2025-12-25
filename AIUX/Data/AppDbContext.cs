afd4d6e3feadbd265874af7445780391f2616414
CodeMeNawh <89380204+CodeMeNawh@users.noreply.github.com>
2025-12-25 15:25:15 +0100

    Add project files.

---
+++ AIUX/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AIUX.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIUX.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User>Users { get; set; }
+    }
+}
